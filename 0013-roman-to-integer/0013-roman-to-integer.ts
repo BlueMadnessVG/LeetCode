95adff100441d5589ba45cc594a67ff85bf55817
Christian Armenta <car_arca@hotmail.com>
2025-11-10 18:29:13 -0700

    Time: 3 ms (90.91%), Space: 63.2 MB (14.69%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -0,0 +1,22 @@
+function romanToInt(s: string): number {
+    const romanCharacters = new Map<string, number>([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000]
+    ]);
+    let sum: number = 0;
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if ( romanCharacters.get(s[i]) < romanCharacters.get(s[i + 1]) ) {
+            sum -= romanCharacters.get(s[i]);
+        }
+        else {
+            sum += romanCharacters.get(s[i])
+        }
+    }
+    return sum +  romanCharacters.get(s[s.length - 1]);
+};
