139db4c06be9385f3a8022d65015c939f7a07463
Christian Armenta <car_arca@hotmail.com>
2025-11-12 17:36:26 -0700

    Time: 43 ms (90.49%), Space: 57.1 MB (86.16%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -1,17 +1,18 @@
 function reverse(x: number): number {
     let revers: number = 0;
-    let xNumber: number = x;   
     let isNegative = false;
 
-    if (x < 0) xNumber = xNumber * -1;
+    if (x < 0) {
+        isNegative = true;
+        x = x * -1;
+    } 
 
-    while(xNumber > 0) {
-        revers = revers * 10 + (xNumber % 10);
-        xNumber = Math.floor(xNumber / 10);
+    while(x > 0) {
+        revers = revers * 10 + (x % 10);
+        x = Math.floor(x / 10);
     }
 
-    if (x < 0) revers = revers * -1;
-    if (revers > Math.pow(2, 31) - 1 || revers < -Math.pow(2, 31)) return 0;
+    if (revers > Math.pow(2, 31) - 1) return 0;
     
-    return revers;
+    return isNegative ? -revers : revers;
 };
