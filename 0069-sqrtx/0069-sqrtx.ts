0cb6b0699bc837d214b4cece0167cd648947a92e
Christian Armenta <car_arca@hotmail.com>
2025-11-20 12:11:14 -0700

    Time: 7 ms (26.85%), Space: 58.4 MB (6.74%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.ts
@@ -0,0 +1,18 @@
+function mySqrt(x: number): number {
+    if (x < 2) return x;
+
+    let left = 1;
+    let right = Math.floor(x / 2);
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        const square = mid * mid;
+
+        if( square == x ) return mid;
+        else if (square < x) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    console.log(right);
+    return right;
+};
