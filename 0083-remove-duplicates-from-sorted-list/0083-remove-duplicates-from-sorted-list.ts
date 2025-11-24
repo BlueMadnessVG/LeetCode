df6e4564c0fecbe7aab6dd8df401995c808f5288
Christian Armenta <car_arca@hotmail.com>
2025-11-24 13:49:22 -0700

    Time: 0 ms (100%), Space: 58.9 MB (11.87%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let result = head;
+
+    while(head && head.next ) {
+        if ( head.val === head.next.val ) {
+            head.next = head.next.next;
+        } else {
+            head = head.next;
+        }
+    }
+
+    return result;
+};
