d357a98a2428f78302f4dea9a8349f21a76b73cc
Christian Armenta <car_arca@hotmail.com>
2025-12-03 16:31:27 -0700

    Time: 1 ms (87.79%), Space: 58.1 MB (16.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,10 +1,10 @@
 function twoSum(nums: number[], target: number): number[] {
     const numMap: Map<number, number> = new Map;
 
-    for(let i = 0; i <= nums.length; i++) {
+    for(let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
         if(numMap.has(complement)) {
-            return [i, numMap.get(complement)]
+            return [i, numMap.get(complement)!]
         }
         numMap.set(nums[i], i);
     }
