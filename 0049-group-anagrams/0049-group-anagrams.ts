bc9870c127bf91157fd93b6f67d175c01403dffc
Christian Armenta <car_arca@hotmail.com>
2025-12-17 20:42:08 -0700

    Time: 43 ms (22.79%), Space: 70.9 MB (9.48%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -0,0 +1,14 @@
+function groupAnagrams(strs: string[]): string[][] {
+    if (strs.length === 1) return [strs];
+
+    let ans = {};
+    for(let s of strs) {
+        let key = s.split('').sort().join('');
+        if (!ans[key]) {
+            ans[key] = [];
+        }
+        ans[key].push(s);
+    }
+
+    return Object.values(ans);
+};
