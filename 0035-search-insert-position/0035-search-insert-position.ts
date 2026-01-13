4f1b5bf9b4198ef5e95541ba76ed1fe061375945
Christian Armenta <car_arca@hotmail.com>
2026-01-13 15:44:32 -0700

    Time: 0 ms (100%), Space: 55.4 MB (60.52%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.ts
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -1,22 +1,13 @@
 function searchInsert(nums: number[], target: number): number {
-    let left: number = 0;
-    let right: number = nums.length - 1;
+    let left = 0;
+    let right = nums.length - 1;
 
-    if( nums[0] > target ) {
-        return 0;
-    }
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
     
-    while(left <= right) {
-        let mid: number = Math.floor((left + right) / 2);
-        let midNum: number = nums[mid];
-
-        if (midNum === target) {
-            return mid;
-        } else if (midNum > target) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] > target) right = mid - 1;
+        else left = mid + 1;
     }
 
     return left;
