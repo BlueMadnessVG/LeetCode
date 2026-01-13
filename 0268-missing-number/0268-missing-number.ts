19d54a2c4929c3d875d99c64e56b1a83296a8abe
Christian Armenta <car_arca@hotmail.com>
2026-01-13 15:41:43 -0700

    Time: 1 ms (72.97%), Space: 58.5 MB (37.91%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.ts
@@ -0,0 +1,9 @@
+function missingNumber(nums: number[]): number {
+    let res = nums.length;
+
+    for (let i = 0; i < nums.length; i++) {
+        res += i - nums[i];
+    }
+
+    return res;
+};
