4eaeef745569a710c759d1ca2105fef1e3cfdc12
Christian Armenta <car_arca@hotmail.com>
2026-01-26 16:01:27 -0700

    Time: 4 ms (91.95%), Space: 65 MB (71.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,13 +1,13 @@
 function isPalindrome(x: number): boolean {
     if (x < 0) return false;
 
-    let reverse = 0;
-    let xcopy = x;
-
+    let reverse: number = 0;
+    let xCopy: number = x; 
+    
     while (x > 0) {
         reverse = (reverse * 10) + (x % 10);
         x = Math.floor(x / 10);
     }
 
-    return reverse === xcopy;
+    return reverse === xCopy;
 };
