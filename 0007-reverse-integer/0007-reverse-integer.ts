d1b9a9a68dcbca6d2cdc22ea25cb41f7395468ef
Christian Armenta <car_arca@hotmail.com>
2026-01-27 16:15:30 -0700

    Time: 56 ms (41.5%), Space: 56.2 MB (94.51%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -1,18 +1,20 @@
 function reverse(x: number): number {
-    let revers: number = 0;
     let isNegative = false;
 
     if (x < 0) {
         isNegative = true;
-        x = x * -1;
-    } 
-
-    while(x > 0) {
-        revers = revers * 10 + (x % 10);
+        x *= -1;
+    }
+    
+    let res: number = 0
+    while (x > 0) {
+        res = (res * 10) + (x % 10);
         x = Math.floor(x / 10);
     }
 
-    if (revers > Math.pow(2, 31) - 1) return 0;
-    
-    return isNegative ? -revers : revers;
+    if (res > Math.pow(2, 31) - 1) {
+        return 0;
+    }
+
+    return isNegative ? -res : res;
 };
