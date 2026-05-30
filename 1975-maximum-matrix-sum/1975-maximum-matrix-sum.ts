b4ef02056a0d6c638452a68177da67b6815203e5
Christian Armenta <car_arca@hotmail.com>
2026-05-29 18:33:17 -0700

    Time: 12 ms (15.38%), Space: 68 MB (7.69%) - LeetHub - Time: O(?), Space: O(?)

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.ts
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.ts
@@ -14,3 +14,7 @@ function maxMatrixSum(matrix: number[][]): number {
 
     return neg % 2 === 0 ? totalSum : totalSum - 2 * minAbs;
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
