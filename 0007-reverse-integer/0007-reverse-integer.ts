3c9b122057e09d3fc72f2ebbbabca91714a23d12
Christian Armenta <car_arca@hotmail.com>
2026-06-19 17:58:22 -0700

    Time: 52 ms (60.58%), Space: 57.7 MB (53.45%) - LeetHub - Time: O(log(x)), Space: O(1)

--- 0007-reverse-integer/0007-reverse-integer.ts
+++ 0007-reverse-integer/0007-reverse-integer.ts
@@ -18,3 +18,7 @@ function reverse(x: number): number {
 
     return isNegative ? -res : res;
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
