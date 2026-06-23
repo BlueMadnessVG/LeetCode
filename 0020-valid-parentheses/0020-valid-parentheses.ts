12f550d3d1ce77d997628f1a1b7d574b11cd62c7
Christian Armenta <car_arca@hotmail.com>
2026-06-22 21:22:22 -0700

    Time: 4 ms (58.76%), Space: 58 MB (54%) - LeetHub - Time: O(n), Space: O(n)

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -19,3 +19,7 @@ function isValid(s: string): boolean {
 
     return stack.length === 0;
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
