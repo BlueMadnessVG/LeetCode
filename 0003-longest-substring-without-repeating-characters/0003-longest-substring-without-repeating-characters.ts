aae8d470eca26af62a6d5e5554e901a539b8d83c
Christian Armenta <car_arca@hotmail.com>
2026-08-14 22:43:43 -0700

    Time: 11 ms (50.92%), Space: 60.4 MB (54.66%) - LeetHub - Time: O(n), Space: O(1)

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -14,3 +14,7 @@ function lengthOfLongestSubstring(s: string): number {
     }
     return maxLen;
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
