985ce945c54cc60315a62ab7ff13088ed0e52375
Christian Armenta <car_arca@hotmail.com>
2026-08-18 21:03:32 -0700

    Time: 3 ms (62.64%), Space: 57.2 MB (44.1%) - LeetHub

---
+++ 0016-two-sum/0016-two-sum.ts
@@ -0,0 +1,15 @@
+function twoSum(nums: number[], target: number): number[] {
+     const pairIdx = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (target - num in pairIdx) {
+            return [i, pairIdx[target - num]];
+        }
+        pairIdx[num] = i;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
