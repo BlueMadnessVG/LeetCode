ba71427c0e11a333383aa22d25f9e51fcfa5276c
Christian Armenta <car_arca@hotmail.com>
2026-09-17 18:54:59 -0700

    Time: 3 ms (83.12%), Space: 60.8 MB (29.22%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.ts
@@ -0,0 +1,23 @@
+var longestValidParentheses = function(s) {
+    const stack = [-1];
+    let max_len = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(i);
+        } else {
+            stack.pop();
+            if (stack.length === 0) {
+                stack.push(i);
+            } else {
+                max_len = Math.max(max_len, i - stack[stack.length - 1]);
+            }
+        }
+    }
+
+    return max_len;
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
